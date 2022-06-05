4de5b89442bb697af82aee85dc54fde0e8c20304
Isaias Hernandez <isaias@MacBook-Pro-de-Isaias.local>
2022-06-05 13:28:33 -0300

    adding URI to connection

--- src/connection/postgresqlconn.js
+++ src/connection/postgresqlconn.js
@@ -6,6 +6,7 @@ const connectionData = {
   database: "dardnfg7fcl5cv",
   password: "ad6945ab866dc16709db3b692b20ca6ba83756b1958e33c9a8969c9c0aad5227",
   port: 5432,
+  URI: "postgres://zkfpflriwzgkul:ad6945ab866dc16709db3b692b20ca6ba83756b1958e33c9a8969c9c0aad5227@ec2-3-211-221-185.compute-1.amazonaws.com:5432/dardnfg7fcl5cv",
 };
 const client = new Client(connectionData);
 
