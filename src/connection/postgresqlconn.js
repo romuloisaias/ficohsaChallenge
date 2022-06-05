a4d10b4b818ccadd219ecea615b1e19cb7549186
Isaias Hernandez <isaias@MacBook-Pro-de-Isaias.local>
2022-06-05 13:14:56 -0300

    setting up connection to postgres in heroku master branch

--- src/connection/postgresqlconn.js
+++ src/connection/postgresqlconn.js
@@ -1,10 +1,10 @@
 const { Client } = require("pg");
 
 const connectionData = {
-  user: "postgres",
-  host: "localhost",
-  database: "ficohsa",
-  password: "postgres",
+  user: "zkfpflriwzgkul",
+  host: "ec2-3-211-221-185.compute-1.amazonaws.com",
+  database: "dardnfg7fcl5cv",
+  password: "ad6945ab866dc16709db3b692b20ca6ba83756b1958e33c9a8969c9c0aad5227",
   port: 5432,
 };
 const client = new Client(connectionData);
